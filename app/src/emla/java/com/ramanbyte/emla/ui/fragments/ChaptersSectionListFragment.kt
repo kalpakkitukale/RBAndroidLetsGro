2fe97ebd053a06a0fece09b8ee89f398af49f646
vinit <vinit.t@ramanbyte.com>
2020-04-14 13:35:00 +0530

    Content Pages added for design

--- app/src/emla/java/com/ramanbyte/emla/ui/fragments/ChaptersSectionListFragment.kt
+++ app/src/emla/java/com/ramanbyte/emla/ui/fragments/ChaptersSectionListFragment.kt
@@ -10,14 +10,14 @@ import com.ramanbyte.R
 /**
  * A simple [Fragment] subclass.
  */
-class ChaptersTopicListFragment : Fragment() {
+class ChaptersSectionListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chapters_topic_list, container, false)
+        return inflater.inflate(R.layout.fragment_chapters_section_list, container, false)
     }
 
 }
