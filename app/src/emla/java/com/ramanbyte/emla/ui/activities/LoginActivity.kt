5ec93056d00705e491cb18b294cb297b4a8b999a
Niraj <niraj.n@ramanbyte.com>
2020-04-14 17:38:24 +0530

    Instruction page added

--- app/src/emla/java/com/ramanbyte/emla/ui/activities/LoginActivity.kt
+++ app/src/emla/java/com/ramanbyte/emla/ui/activities/LoginActivity.kt
@@ -2,11 +2,15 @@ package com.ramanbyte.emla.ui.activities
 
 import android.app.Activity
 import android.content.Intent
+import android.view.View
+import androidx.fragment.app.FragmentTransaction
 import com.ramanbyte.R
 import com.ramanbyte.base.BaseActivity
 import com.ramanbyte.databinding.ActivityLoginBinding
 import com.ramanbyte.emla.base.di.authModuleDependency
+import com.ramanbyte.emla.ui.fragments.PreAssessmentTestFragment
 import com.ramanbyte.emla.view_model.LoginViewModel
+import com.ramanbyte.utilities.AppLog
 
 /**
  * @AddedBy Vinay Kumbhar <vinay.k@ramanbyte.com>
@@ -19,6 +23,16 @@ class LoginActivity:BaseActivity<ActivityLoginBinding,LoginViewModel>(authModule
     override fun layoutId(): Int  = R.layout.activity_login
 
     override fun initiate() {
+
+        layoutBinding.apply {
+            btnLogin.setOnClickListener(View.OnClickListener {
+                AppLog.infoLog("LoginClick")
+                val fragment = PreAssessmentTestFragment()
+                val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+                transaction.replace(R.id.frameLayout, fragment, "PreAssessmentTestFragment")
+                transaction.commit()
+            })
+        }
 
     }
 
