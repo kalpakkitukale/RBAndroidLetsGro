7160cb5f9281f362116d709685a13cf33f98166c
vinay <vinay.k@ramanbyte.com>
2020-04-17 13:41:41 +0530

    added userId in course filter

--- app/src/emla/java/com/ramanbyte/emla/view_model/CoursesViewModel.kt
+++ app/src/emla/java/com/ramanbyte/emla/view_model/CoursesViewModel.kt
@@ -145,8 +145,8 @@ class CoursesViewModel(mContext: Context) : BaseViewModel(mContext = mContext) {
             val isFilterSelected = hasFilter()
 
             filterRequestModel = CoursesRequest().apply {
-                userId = if (isFilterSelected) 0 else userData?.userId!!
-                userType = tempFilterModel.userType
+                userId = /*if (isFilterSelected) 0 else */ userData?.userId!!
+                userType = userData?.userType!!
                 programId = tempFilterModel.programId
                 specializationId = tempFilterModel.specializationId
                 patternId = tempFilterModel.patternId
@@ -162,14 +162,14 @@ class CoursesViewModel(mContext: Context) : BaseViewModel(mContext = mContext) {
         }
     }
 
-    fun hasFilter(): Boolean {
+    private fun hasFilter(): Boolean {
         return (tempFilterModel.userType.isNotEmpty() ||
                 tempFilterModel.programId != 0 ||
                 tempFilterModel.patternId != 0 ||
                 tempFilterModel.specializationId != 0)
     }
 
-    fun getFilterState(): Boolean {
+    private fun getFilterState(): Boolean {
         return (filterRequestModel.userType.isNotEmpty() ||
                 filterRequestModel.programId != 0 ||
                 filterRequestModel.patternId != 0 ||
@@ -178,7 +178,7 @@ class CoursesViewModel(mContext: Context) : BaseViewModel(mContext = mContext) {
 
     fun onClearFilterClick(view: View) {
         tempFilterModel.apply {
-            userId = userData?.userId!!
+            /* userId = userData?.userId!!*/
             userType = ""
             programId = 0
             specializationId = 0
