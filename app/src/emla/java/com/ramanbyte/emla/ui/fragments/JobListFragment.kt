16554de2025a640978b6a1c1b484c29207d50b12
Mansi Manakiki Mody <mansi.m@ramanbyte.com>
2021-08-27 11:55:34 +0530

    click events

--- app/src/emla/java/com/ramanbyte/emla/ui/fragments/JobListFragment.kt
+++ app/src/emla/java/com/ramanbyte/emla/ui/fragments/JobListFragment.kt
@@ -1,6 +1,10 @@
 package com.ramanbyte.emla.ui.fragments
 
 import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.EditorInfo
 import androidx.lifecycle.Observer
 import com.ramanbyte.R
 import com.ramanbyte.base.BaseFragment
@@ -8,6 +12,7 @@ import com.ramanbyte.databinding.FragmentJobListBinding
 import com.ramanbyte.emla.adapters.JobListAdapter
 import com.ramanbyte.emla.view_model.JobsViewModel
 import com.ramanbyte.utilities.AlertDialog
+import com.ramanbyte.utilities.KEY_BLANK
 import com.ramanbyte.utilities.KEY_SKILL_ID
 import com.ramanbyte.utilities.ProgressLoader
 
@@ -68,6 +73,55 @@ class JobListFragment :
             getJobsList()!!.observe(this@JobListFragment, Observer {
                 jobListAdapter?.submitList(it)
             })
+        }
+
+        layoutBinding.apply {
+            edtJobSearch.apply {
+                addTextChangedListener(object : TextWatcher {
+                    override fun afterTextChanged(searckKey: Editable?) {
+                    }
+
+                    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    }
+
+                    override fun onTextChanged(
+                        searckKey: CharSequence?,
+                        p1: Int,
+                        p2: Int,
+                        p3: Int
+                    ) {
+                        imageViewSearchClear.visibility = if (searckKey!!.isNotEmpty()) {
+                            View.VISIBLE
+                        } else {
+                            View.INVISIBLE
+                        }
+                    }
+                })
+
+                setOnEditorActionListener { _, actionId, _ ->
+                    if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                        val searchStr = edtJobSearch.text.toString()
+                        if (searchStr.isNotEmpty()) {
+                            viewModel.apply {
+                                viewModel.searchJobQuery.postValue(searchStr)
+                            }
+                        }
+                        true
+                    } else false
+                }
+
+            }
+
+            imageViewSearchClear.apply {
+                setOnClickListener(View.OnClickListener {
+                    edtJobSearch.setText(KEY_BLANK)
+                    viewModel.apply {
+                        searchJobQuery.postValue(KEY_BLANK)
+                    }
+                })
+
+            }
+
         }
     }
 
