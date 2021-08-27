d22eab5a2d133624fadafe4a79035b4a70a12bab
Mansi Manakiki Mody <mansi.m@ramanbyte.com>
2021-08-27 13:10:49 +0530

    click events

--- app/src/emla/java/com/ramanbyte/emla/models/response/SkillsModel.kt
+++ app/src/emla/java/com/ramanbyte/emla/models/response/SkillsModel.kt
@@ -4,24 +4,22 @@ import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
 import com.google.gson.annotations.SerializedName
 import com.ramanbyte.BR
-import com.ramanbyte.utilities.KEY_BLANK
 
 class SkillsModel : BaseObservable() {
 
     @SerializedName("chapterId")
     var skillId: Int? = 0
 
-    @SerializedName("chapterName")
     @Bindable
-    var skillName: String? = KEY_BLANK
+    var skillName: String? = "Website Development"
+        //KEY_BLANK
         set(value) {
             field = value
             notifyPropertyChanged(BR.skillName)
         }
 
-    @SerializedName("totalSectionCount")
     @Bindable
-    var totalNumberOfJobs: Int? = 0
+    var totalNumberOfJobs: Int? = 150
         set(value) {
             field = value
             notifyPropertyChanged(BR.totalNumberOfJobs)
